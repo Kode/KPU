e92fd4ebfc5ba62674a69d05ab14642a88da3db2
Robert Konrad <git2016@robdangero.us>
2018-12-31 04:37:17 +0100

    Run within Kore

--- driver/main.cpp
+++ driver/main.cpp
@@ -1,3 +1,9 @@
+#include <Kore/pch.h>
+
+#include <Kore/Graphics4/Graphics.h>
+#include <Kore/Log.h>
+#include <Kore/System.h>
+
 #include "../obj_dir/Valu.h"
 
 #include <stdio.h>
@@ -8,7 +14,38 @@ const int textsize = 1024 * 1024;
 static unsigned char text[textsize];
 const int textoffset = 0x100000 << 2;
 
-int main() {
+static Valu top;
+
+static void update() {
+	if (Verilated::gotFinish()) {
+		Kore::System::stop();
+	}
+
+	top.clk = 1;
+	top.eval();
+	top.clk = 0;
+	top.eval();
+
+	if (top.memop == 1) {
+		Kore::log(Kore::Info, "Preparing address %x.\n", top.memaddress);
+		unsigned int* addr = top.memaddress >= textoffset ? (unsigned int*)&text[top.memaddress - textoffset] : (unsigned int*)&data[top.memaddress];
+		top.memindata = *addr;
+		Kore::log(Kore::Info, "Loaded %d from address %x.\n", *addr, top.memaddress);
+	}
+	else if (top.memop == 2) {
+		unsigned int* addr = top.memaddress >= textoffset ? (unsigned int*)&text[top.memaddress - textoffset] : (unsigned int*)&data[top.memaddress];
+		*addr = top.memoutdata;
+	}
+
+	Kore::Graphics4::begin();
+	Kore::Graphics4::clear(Kore::Graphics4::ClearColorFlag);
+	Kore::Graphics4::end();
+	Kore::Graphics4::swapBuffers();
+}
+
+int kore(int argc, char** argv) {
+	Kore::System::init("KPU", 640, 480);
+
 	for (int i = 0; i < datasize; ++i) {
 		data[i] = 0;
 	}
@@ -34,8 +71,6 @@ int main() {
 		fclose(file);
 	}
 
-	Valu top;
-
 	// Reset
 	top.rst = 1;
 	top.clk = 0;
@@ -44,23 +79,9 @@ int main() {
 	top.eval();
 	top.rst = 0;
 
-	for (int i = 0; i < 100000 && !Verilated::gotFinish(); ++i) {
-		top.clk = 1;
-		top.eval();
-		top.clk = 0;
-		top.eval();
+	Kore::System::setCallback(update);
+	Kore::System::start();
 
-		if (top.memop == 1) {
-			printf("Preparing address %x.\n", top.memaddress);
-			unsigned int* addr = top.memaddress >= textoffset ? (unsigned int*)&text[top.memaddress - textoffset] : (unsigned int*)&data[top.memaddress];
-			top.memindata = *addr;
-			printf("Loaded %d from address %x.\n", *addr, top.memaddress);
-		}
-		else if (top.memop == 2) {
-			unsigned int* addr = top.memaddress >= textoffset ? (unsigned int*)&text[top.memaddress - textoffset] : (unsigned int*)&data[top.memaddress];
-			*addr = top.memoutdata;
-		}
-	}
 	top.final();
 
 	return 0;
--- korefile.js
+++ korefile.js
@@ -6,9 +6,6 @@ project.addFiles('obj_dir/**');
 project.addIncludeDir('verilator/include');
 project.addFiles('verilator/include/verilated.cpp');
 
-project.kore = false;
-project.cmd = true;
-
 project.setDebugDir('asm');
 
 resolve(project);
